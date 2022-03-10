e69283d2008c90e2a6e8f3dbed07d234e3de1879
Nishant Bhandari <79033833+nishant-out@users.noreply.github.com>
2022-03-10 10:47:34 +0530

    Update q4.java

--- Lab_2/q4.java
+++ Lab_2/q4.java
@@ -2,6 +2,25 @@
 // given a sorted array that can contain duplicate elements also
 // you have to find no. of occurence of a key element in the array in log( n ) time
 
+/*
+Approach 1:
+   
+   1. find the key element in the array
+   
+   now more key elements can exist in left half and right half of the array
+   so we have to find those elements in log( n ) time also
+   
+   2. find a point in the left half of the array 
+		i.e. ith index from where my i -1th index < key
+   
+   3. illy, in the right half of the array
+   		i.e here j +1th index element > key
+
+	
+	4.	total occurence = j -i +1
+
+*/
+
 import java.util.*;  
 
 public class q4{
@@ -23,7 +42,16 @@ public class q4{
       System.out.println( "Enter Element to Search: " );
       int key = sc.nextInt();
       
-      int copies = bs( arr, 0, arr.length -1, key );
+      int mid = bs( arr, 0, arr.length -1, key );
+      
+      
+      int st = focc( arr, 0, mid, arr[ mid ] );
+      
+      
+      
+      
+      
+      
       
       if( copies == 0 )
          System.out.println( "\nKey not Found" );
@@ -31,6 +59,26 @@ public class q4{
          System.out.println( "\nKey Found " + copies + " times" );
       
    }
+   
+   
+   public static int focc( int[] arr, int st, int en, int key ){
+   			
+   		if( st <= en ){
+   		
+   			int mid = ( st +en ) /2;
+   		
+   			if( arr[ mid ] == key )	//	THis can be a possible answer
+   				if( arr[ mid -1 ] < key )	//	ANswer found
+   					return mid;
+   				else
+   					return focc( arr, st, mid -1, key );
+   				
+   			else
+   				return focc( arr, st, mid, key );
+   		}
+   }
+   
+   
    
    public static int bs( int[] arr, int st, int en, int key ){
       int cnt = 0;
@@ -40,17 +88,14 @@ public class q4{
          int mid = (st +en) /2;
          
          if( arr[ mid ] == key ){
-            cnt++;
-            cnt += bs( arr, st, mid -1, key );
-            cnt += bs( arr, mid +1, en, key );
+            return mid;
          }
          else if( arr[ mid ] > key )
-            cnt += bs( arr, st, mid -1, key );
+            return bs( arr, st, mid -1, key );
             
          else
-            cnt += bs( arr, mid +1, en, key );
+            return bs( arr, mid +1, en, key );
       }
       
-      return cnt;
    }
 }
