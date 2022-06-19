fb76fb75ec436d1b4f5bdd50d25757b5094813d4
Nishant Bhandari <79033833+nishant-out@users.noreply.github.com>
2022-06-19 12:53:19 +0530

    Update p16.java

--- daa/p16.java
+++ daa/p16.java
@@ -1,13 +1,11 @@
 import java.util.*;
 
-//  Cycle detection in Directed gp
-
-class p16{
+class Main{
    
     private int V;  //  No of vertices in gp
     private List<List<Integer>> adj;    //  Representing gp using adjacency List
    
-    p16( int V ){
+    Main( int V ){
        
         this.V = V;
         adj = new ArrayList<>( V );
@@ -33,69 +31,39 @@ class p16{
         }
     }
    
-    boolean hlp( int v, boolean[] visited, boolean[] recStack ){
-       
-        //  A Cycle is detected from node v, if
-        //      a node x is encountered i.e. v ---> x , & recStack[ x ] == true
-        //          then, Cycle detected
-       
-        if( recStack[ v ] ) //  If node v is already visited from path of 1st source node, then return true OR cycle detected
-            return true;
-        if( visited[ v ] )  //  If node v is already visited (& not in current path) then, no Cycle can be formed from this node
-            return false;
-       
-        recStack[ v ] = true;
-        visited[ v ] = true;
-       
-        for( Integer e : adj.get( v ) ){
-           
-            //  For all adjacenct nodes from current node
-            //      recurse down the problem
-           
-            if( hlp( e, visited, recStack ) )      return true;
-        }
-       
-        recStack[ v ] = false;
-       
-        return false;
-    }
-   
-    boolean isCyclic(){
-       
-        //  For each node
-        //      check if from that node a Cycle is made or not
-        boolean ans = false;
-        boolean[] visited = new boolean[ V ];
-        boolean[] recStack = new boolean[ V ];
-       
-        for( int v = 0; v < V; v++ ){
-            ans = hlp( v, visited, recStack );
-           
-            if( ans )   return true;
+    boolean hasPath( int src, int dest ) {
+        
+        if( src == dest )   return true;
+        
+        List<Integer> nearV = adj.get( src );
+        
+        for( int e = 0; e < nearV.size(); e++ ){
+            
+            if( hasPath( nearV.get( e ), dest ) )
+                return true;
         }
         return false;
     }
    
-public static void main(String[] args) {
-   
-   int nov = 3;    //  No. of vertices
-   Main gp = new Main( nov );
-   
-   gp.addEdge( 1, 2 );
-   gp.addEdge( 2, 0 );
-   gp.addEdge( 0, 1 );
-   
-   gp.gpShow();
-
-   System.out.println( gp.isCyclic() );
-   }
+    public static void main(String[] args) {
+       
+       int nov = 8;    //  No. of vertices
+       Main gp = new Main( nov );
+       
+      gp.addEdge( 1, 2 );
+      gp.addEdge( 1, 3 );
+      gp.addEdge( 0, 1 );
+      gp.addEdge( 2, 3 );
+      gp.addEdge( 3, 4 );
+      gp.addEdge( 4, 1 );
+       
+      gp.addEdge( 5, 6 );
+      gp.addEdge( 6, 7 );
+      gp.addEdge( 7, 5 );
+       
+       gp.gpShow();
+    
+      System.out.println( gp.hasPath( 0, 6 ) );
+    }
 }
-
-
-
-
-
-
-
-
 
